1da590a7040c5fc12e04062e4e070ae814d1e8b1
alphadzingina <alphaadx6@gmail.com>
2022-05-28 15:59:30 +0100

    pass data to ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,12 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2022, 5, 28);
-    const expenseTitle = "Car Insurance";
-    const expnseAmount = "$294.67"
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item_description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item_price">{expnseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item_price">{props.amount}</div>
             </div>
         </div>
     );
